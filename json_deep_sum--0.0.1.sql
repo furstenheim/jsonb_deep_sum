8a11870a5522c45abe86d82a257f4cdf53a6ab6e
gabriel <furstenheim@gmail.com>
2017-06-04 10:53:20 +0200

    Read json

--- json_deep_sum--0.0.1.sql
+++ json_deep_sum--0.0.1.sql
@@ -1,6 +1,6 @@
 \echo Use "Create extension json_deep_sum to load this file. \quit
 
-create function json_list(mjson json)
-Returns integer
+create function json_list(mjson jsonb)
+Returns integer 
 as '$libdir/json_deep_sum'
 language c immutable strict;
--- json_deep_sum.c
+++ json_deep_sum.c
@@ -1,13 +1,16 @@
 #include "postgres.h"
+#include "catalog/pg_type.h"
 #include "fmgr.h"
 #include "utils/builtins.h"
-
+#include "utils/jsonb.h"
 
 PG_MODULE_MAGIC;
 
-PG_FUNCTION_INFO_V1(json_deep_sum);
+PG_FUNCTION_INFO_V1(json_list);
 Datum
 json_list(PG_FUNCTION_ARGS)
 {
+ Jsonb *jb = PG_GETARG_JSONB(0);
+
  PG_RETURN_INT16(1);
 }
