0900b848c55c2d943faa1df92203b03ee169c208
gabriel <furstenheim@gmail.com>
2017-06-04 21:55:47 +0200

    Get property from json

--- json_deep_sum.c
+++ json_deep_sum.c
@@ -11,26 +11,20 @@ json_list(PG_FUNCTION_ARGS)
 {
  Jsonb *jb1 = PG_GETARG_JSONB(0);
  Jsonb *jb2 = PG_GETARG_JSONB(1);
- JsonbIterator *it1, *it2; 
-/*Jsonb *out = palloc(VARSIZE(jb1);
- uint32 r;
- JsonbParseState *state = NULL;
- JsonbValue *v = NULL;
- if (!JB_ROOT_IS_OBJECT(jb2))
-    ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("Can only sum objects")));
+ text *key;
+ JsonbValue *v;
+ JsonbValue kval;
+ key = cstring_to_text("a");
 
- it1 = JsonbIteratorInit(&jb2->root);
- 
- while ((r = JsonbIterator(&it, &v, false)) != 0)
- {
-  if ((r == WJB_KEY) && v.type === jbvString 
- }*/
- bool isContained;
- it1 = JsonbIteratorInit(&jb1->root);
- it2 = JsonbIteratorInit(&jb2->root);
- isContained = JsonbDeepContains(&it1, &it2);
-    
+ kval.type = jbvString;
+ kval.val.string.val = VARDATA_ANY(key);
+ kval.val.string.len = VARSIZE_ANY_EXHDR(key);
+ if (!JB_ROOT_IS_OBJECT(jb1))
+     PG_RETURN_NULL();
 
+ v = findJsonbValueFromContainer(&jb1->root, JB_FOBJECT, &kval);
 
- PG_RETURN_BOOL(isContained);
+ if (v != NULL)
+    PG_RETURN_JSONB(JsonbValueToJsonb(v));
+ PG_RETURN_NULL();
 }
